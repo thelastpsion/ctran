3f335f537ba39fbf2ea7876793ebef1764e5b05e
Alex Brown <mail@alexbrown.info>
2024-03-01 16:51:22 +0000

    Remove GetParams() as it's now handled elsewhere

--- ctran.pp
+++ ctran.pp
@@ -7,65 +7,9 @@ uses
 var
     strFilename : String;
     CatLexer : TPsionOOLexer;
-    boolExternal : Boolean;
-    boolGenG : Boolean;
+    // boolExternal : Boolean;
+    // boolGenG : Boolean;
     params : TPsionSDKAppParams;
-
-procedure GetParams();
-var
-    cur, tot : Integer;
-    thisParam : String;
-    flgFoundName: Boolean = false;
-begin
-    tot := paramCount();
-    cur := 1;
-
-    while cur <= tot do
-    begin
-        thisParam := paramStr(cur);
-        if thisParam[1] = '-' then begin
-            case UpCase(thisParam[2]) of
-                'E': begin
-                    writeln('external includes found');
-                    boolExternal := true;
-                end;
-                'G': begin
-                    writeln('generate .G files');
-                    boolGenG := true;
-                end;
-                'X': begin
-                    writeln('generate .EXT files');
-                    boolGenG := true;
-                end;
-                'V': begin
-                    writeln('verbose');
-                end;
-                else begin
-                    writeln('Nope!');
-                    exit();
-                end;
-            end;
-            if length(thisParam) > 2 then begin
-                thisParam := copy(thisParam, 3);
-            end else begin
-                thisParam := '';
-            end;
-            writeln('Value: ', thisParam);
-        end else begin
-            if flgFoundName then begin
-                writeln('Too many items without a switch!');
-                exit();
-            end;
-            writeln('Found name of file to process: ', thisParam);
-            flgFoundName := true;
-            strFilename := thisParam;
-        end;
-        inc(cur);
-        // TODO: Avoid incrementing if a -thing is followed by another -thing
-        // TODO: Check for too many items without a switch
-        // TODO: is it worth catering for `-s value` as well as `-svalue`? 
-    end;
-end;
 
 procedure HelpText();
 var

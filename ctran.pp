ec6a57b0e75198a5e3f79cc03aacd69754569f50
Alex Brown <mail@alexbrown.info>
2024-03-16 21:19:34 +0000

    fix(MakeC): Correct number of NULLs, method count, method start index

--- ctran.pp
+++ ctran.pp
@@ -480,10 +480,11 @@ var
     methodReplace_list : array of TPsionOOMethodEntry;
     metaclass : TStringList;
     metaclass_method_id : Integer;
-    flg : Boolean;
+    flgFoundFirst : Boolean;
+    flgExists : Boolean;
     i : Integer;
+    count_possible_nulls : Integer;
 begin
-    // FIX: Multiple broken things here
     SetLength(Result.Methods, 0);
     for method in class_item.Methods do
     begin
@@ -493,7 +494,8 @@ begin
         end;
     end;
 
-    flg := false;
+    count_possible_nulls := 0;
+    flgFoundFirst := false;
     metaclass := MakeMetaclass(class_item);
 
     Result.StartIndex := metaclass.Count;
@@ -501,30 +503,41 @@ begin
         for metaclass_method_id := 0 to metaclass.Count - 1 do
         begin
             s := metaclass[metaclass_method_id];
+            flgExists := false;
             for i := 0 to length(methodReplace_list) - 1 do
             begin
                 if s = methodReplace_list[i].Name then begin
-                    WriteLn('*** ', class_item.Name, ': Earliest replaced method is ', s);
-                    Result.Methods := concat(Result.Methods, [methodReplace_list[i]]);
-                    if not flg then begin
+                    if flgFoundFirst then begin
+                        if count_possible_nulls > 0 then begin
+                            SetLength(Result.Methods, length(Result.Methods) + count_possible_nulls); // Adds blank entries
+                            count_possible_nulls := 0;
+                        end;
+                    end else begin
+                        // WriteLn('*** ', class_item.Name, ': Earliest replaced method is ', s, ' (', metaclass_method_id, ')');
                         Result.StartIndex := metaclass_method_id;
-                        flg := true;
+                        flgFoundFirst := true;
                     end;
-                end else if flg then begin
-                    method.Name := '';
-                    method.MethodType := methodReplace;
-                    method.ForwardRef := '';
-                    Result.Methods := concat(Result.Methods, [method]);
+                    Result.Methods := concat(Result.Methods, [methodReplace_list[i]]);
+                    flgExists := true;
+                    break;
                 end;
+            end;
+            if (not flgExists) and (flgFoundFirst) then begin
+                inc(count_possible_nulls);
             end;
         end;
     end;
-    Result.Methods := concat(Result.Methods, methodAdd_list);
+
+    if length(methodAdd_list) > 0 then begin
+        // WriteLn('Adding trailing nulls (', count_possible_nulls, ')');
+        SetLength(Result.Methods, length(Result.Methods) + count_possible_nulls);
+        Result.Methods := concat(Result.Methods, methodAdd_list);
+    end;
 end;
 
 procedure MakeC(par : TPsionOOLexer);
 var
-    i : Integer;
+    // i : Integer;
     class_item : TPsionOOClass;
     s : String;
     ts : TStringList;

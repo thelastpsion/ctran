349f6767385a42b779f79fc0ee787358a141bbb7
Alex Brown <github@alexbrown.info>
2024-09-18 23:45:19 +0100

    Refactor Reconstruct(); Change "lex" to "par"

--- PsionOOCatDiagnostics.pas
+++ PsionOOCatDiagnostics.pas
@@ -4,13 +4,13 @@ unit PsionOOCatDiagnostics;
 interface
 uses sysutils, PsionOOParser;
 
-procedure PrintArray(lex : TPsionOOParser);
-procedure ShowTree(lex : TPsionOOParser);
-procedure Reconstruct(lex: TPsionOOParser);
+procedure PrintArray(par: TPsionOOParser);
+procedure ShowTree(par: TPsionOOParser);
+procedure Reconstruct(par: TPsionOOParser);
 
 implementation
 
-procedure PrintArray(lex : TPsionOOParser);
+procedure PrintArray(par: TPsionOOParser);
 var
     s: String;
     tok : TToken;
@@ -19,61 +19,53 @@ begin
     Writeln(' Line | Pos | Token Type     | Literal');
     Writeln('------+-----+----------------+-------------');
 
-    for tok in lex.Tokens do
+    for tok in par.Tokens do
     begin
         Str(tok.TType, s); // Because you can't simply use an enum in format()
         Writeln(format(' %4d | %3d | %-14s | %s', [tok.LineNum, tok.LinePos, s, tok.Literal]));
     end;
 
     Writeln;
-    Writeln('Length: ', Length(lex.Tokens));
+    Writeln('Length: ', Length(par.Tokens));
 end;
 
-procedure ShowTree(lex : TPsionOOParser);
+procedure ShowTree(par: TPsionOOParser);
 var
     i : integer;
     s: String;
+    class_item: TPsionOOClass;
     constant_entry: TPsionOOConstantEntry;
     method_entry: TPsionOOMethodEntry;
 begin
     Writeln;
     Writeln('EXTERNALs');
     Writeln('---------');
-    for i := 0 to lex.ExternalList.Count - 1 do
-    begin
-        Writeln(lex.ExternalList[i]);
-    end;
+    for s in par.ExternalList do WriteLn(s);
 
     Writeln;
     Writeln('INCLUDEs');
     Writeln('--------');
-    for i := 0 to lex.IncludeList.Count - 1 do
-    begin
-        Writeln(lex.Includelist[i]);
-    end;
+    for s in par.IncludeList do WriteLn(s);
 
     Writeln;
     Writeln('REQUIREs');
     Writeln('--------');
-    for i := 0 to lex.RequireList.Count - 1 do
-    begin
-        Writeln(lex.RequireList[i]);
-    end;
+    for s in par.RequireList do WriteLn(s);
 
     Writeln;
     Writeln('CLASSes');
     Writeln('-------');
 
-    for i := 0 to length(lex.ClassList) - 1 do
+    for class_item in par.ClassList do
     begin
-        Write('Name: ', lex.ClassList[i].Name);
-        if lex.ClassList[i].Parent = '' then begin
+        Write('Name: ', class_item.Name);
+        if class_item.Parent = '' then begin
             Writeln(' (root class)');
         end else begin
-            Writeln(' (inherits from ', lex.classList[i].Parent, ')');
+            Writeln(' (inherits from ', class_item.Parent, ')');
         end;
 
-        for method_entry in lex.ClassList[i].Methods do
+        for method_entry in class_item.Methods do
         begin
             Write('  ', method_entry.MethodType, ' ', method_entry.Name);
             if method_entry.ForwardRef <> '' then
@@ -83,26 +75,20 @@ begin
             WriteLn;
         end;
 
-        Writeln('  Types (',  lex.ClassList[i].ClassTypes.Count, '):');
-        for s in lex.ClassList[i].ClassTypes do
-        begin
-            Writeln('    ', s);
-        end;
+        Writeln('  Types (',  class_item.ClassTypes.Count, '):');
+        for s in class_item.ClassTypes do Writeln('    ', s);
 
-        Writeln('  Constants (', length(lex.ClassList[i].ClassConstants), '):');
-        for constant_entry in lex.ClassList[i].ClassConstants do
+        Writeln('  Constants (', length(class_item.ClassConstants), '):');
+        for constant_entry in class_item.ClassConstants do
         begin
             Writeln('    ', constant_entry.Name, ' ', constant_entry.Value);
         end;
 
-        Writeln('  Property (', lex.ClassList[i].ClassProperty.Count,'):');
-        for s in lex.ClassList[i].ClassProperty do
-        begin
-            Writeln('    ', s);
-        end;
+        Writeln('  Property (', class_item.ClassProperty.Count,'):');
+        for s in class_item.ClassProperty do Writeln('    ', s);
 
-        if lex.ClassList[i].HasMethod then WriteLn('  HAS_METHOD set');
-        if lex.ClassList[i].HasProperty then WriteLn('  HAS_PROPERTY set');
+        if class_item.HasMethod then WriteLn('  HAS_METHOD set');
+        if class_item.HasProperty then WriteLn('  HAS_PROPERTY set');
     end;
 
     Writeln;
@@ -112,98 +98,103 @@ begin
     WriteLn(' Element | Type        | Index');
     WriteLn('---------+-------------+-------');
 
-    for i := 0 to length(lex.ElementList) - 1 do
+    for i := 0 to length(par.ElementList) - 1 do
     begin
-        Str(lex.ElementList[i].ElementType, s);
-        WriteLn(format('    %04d | %-11s | %04d', [i, s, lex.ElementList[i].index]));
+        Str(par.ElementList[i].ElementType, s);
+        WriteLn(format('    %04d | %-11s | %04d', [i, s, par.ElementList[i].index]));
     end;
 end;
 
-procedure Reconstruct(lex: TPsionOOParser);
+//
+// Reconstruct Category File
+//
+
+procedure ReconstructClass(cls: TPsionOOClass);
 var
     s: String;
+    mtd: TPsionOOMethodEntry;
+    cst: TPsionOOConstantEntry;
+begin
+    Write('CLASS ', cls.Name, ' ');
+    if cls.Parent <> '' then Write(cls.Parent);
+    WriteLn;
+    WriteLn('{');
+
+    for mtd in cls.Methods do
+    begin
+        case mtd.MethodType of
+            methodReplace:  Write('REPLACE ');
+            methodDefer:    Write('DEFER ');
+            methodAdd:      Write('ADD ');
+            methodDeclare:  Write('DECLARE ');
+            else            Write(mtd.MethodType, ' ');
+        end;
+        Write(mtd.Name);
+        if mtd.ForwardRef <> '' then begin
+            Write(' = ', mtd.ForwardRef);
+        end;
+        WriteLn;
+    end;
+
+    if cls.HasMethod then WriteLn('HAS_METHOD');
+    if cls.HasProperty then WriteLn('HAS_PROPERTY');
+    if length(cls.ClassConstants) > 0 then begin
+        WriteLn('CONSTANTS');
+        Writeln('{');
+        for cst in cls.ClassConstants do WriteLn(cst.Name, ' ', cst.Value);
+        Writeln('}');
+    end;
+
+    if cls.ClassTypes.Count > 0 then begin
+        WriteLn('TYPES');
+        Writeln('{');
+        for s in cls.ClassTypes do WriteLn(s);
+        Writeln('}');
+    end;
+
+    if cls.ClassProperty.Count > 0 then begin
+        Write('PROPERTY');
+        if cls.PropertyAutodestroyCount > 0 then
+            Write(' ', cls.PropertyAutodestroyCount);
+        WriteLn;
+        Writeln('{');
+        for s in cls.ClassProperty do WriteLn(s);
+        Writeln('}');
+    end;
+
+    WriteLn('}');
+end;
+
+procedure Reconstruct(par: TPsionOOParser);
+var
     element : TPsionOOFileElement;
-    method: TPsionOOMethodEntry;
-    constant_entry: TPsionOOConstantEntry;
 begin
     Writeln;
     Writeln('Reconstructed File');
     Writeln('------------------');
 
-    case lex.CategoryType of
+    case par.CategoryType of
         catName:    Write('NAME');
         catImage:   Write('IMAGE');
         catLibrary: Write('LIBRARY');
-        else        Write(lex.CategoryType);
+        else        Write(par.CategoryType);
     end;
-    WriteLn(' ', LowerCase(lex.ModuleName));
+    WriteLn(' ', LowerCase(par.ModuleName));
 
-    for element in lex.ElementList do
+    for element in par.ElementList do
     begin
         case element.ElementType of
             incExternal: begin
-                WriteLn('EXTERNAL ', lex.ExternalList[element.index]);
+                WriteLn('EXTERNAL ', par.ExternalList[element.index]);
             end;
             incInclude: begin
-                WriteLn('INCLUDE ', lex.IncludeList[element.index]);
+                WriteLn('INCLUDE ', par.IncludeList[element.index]);
             end;
             incRequire: begin
-                WriteLn('REQUIRE ', lex.RequireList[element.index]);
+                WriteLn('REQUIRE ', par.RequireList[element.index]);
             end;
             incClass: begin
-                Write('CLASS ', lex.ClassList[element.index].Name, ' ');
-                if lex.ClassList[element.index].Parent <> '' then
-                    Write(lex.ClassList[element.index].Parent);
-                WriteLn;
-                WriteLn('{');
-                for method in lex.ClassList[element.index].Methods do
-                begin
-                    case method.MethodType of
-                        methodReplace: Write('REPLACE ');
-                        methodDefer:   Write('DEFER ');
-                        methodAdd:     Write('ADD ');
-                        methodDeclare: Write('DECLARE ');
-                        else           Write(method.MethodType, ' ');
-                    end;
-                    Write(method.Name);
-                    if method.ForwardRef <> '' then begin
-                        Write(' = ', method.ForwardRef);
-                    end;
-                    WriteLn;
-                end;
-                if lex.ClassList[element.index].HasMethod then WriteLn('HAS_METHOD');
-                if lex.ClassList[element.index].HasProperty then WriteLn('HAS_PROPERTY');
-                if length(lex.ClassList[element.index].ClassConstants) > 0 then begin
-                    WriteLn('CONSTANTS');
-                    Writeln('{');
-                    for constant_entry in lex.ClassList[element.index].ClassConstants do
-                    begin
-                        WriteLn(constant_entry.Name, ' ', constant_entry.Value);
-                    end;
-                    Writeln('}');
-                end;
-                if lex.ClassList[element.index].ClassTypes.Count > 0 then begin
-                    WriteLn('TYPES');
-                    Writeln('{');
-                    for s in lex.ClassList[element.index].ClassTypes do
-                    begin
-                        WriteLn(s);
-                    end;
-                    Writeln('}');
-                end;
-                if lex.ClassList[element.index].ClassProperty.Count > 0 then begin
-                    Write('PROPERTY');
-                    if lex.ClassList[element.index].PropertyAutodestroyCount > 0 then
-                        Write(' ', lex.ClassList[element.index].PropertyAutodestroyCount);
-                    WriteLn;
-                    Writeln('{');
-                    for s in lex.ClassList[element.index].ClassProperty do
-                    begin
-                        WriteLn(s);
-                    end;
-                    Writeln('}');
-                end;
-                WriteLn('}');
+                ReconstructClass(par.ClassList[element.index]); // INFO: Spun off to make it more readable and hopefully faster
             end;
         end;
     end;

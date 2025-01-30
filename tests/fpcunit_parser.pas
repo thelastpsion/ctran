8af0aa3dde5e9dffbad9df5a1383bfb5eeba1b80
Alex Brown <github@alexbrown.info>
2025-01-30 16:44:40 +0000

    Added lightweight token file parser

--- tests/fpcunit_parser.pas
+++ tests/fpcunit_parser.pas
@@ -8,6 +8,7 @@ uses
     testregistry,
     PsionOOParser,
     SysUtils,
+    TypInfo,
     Classes;
 
 type
@@ -19,6 +20,7 @@ type
     TPsionOOParserWrap = class(TPsionOOParser)
         private
             procedure _AddToken(toktype: TTokenType; tokliteral: String);
+            procedure _AddTokensFromFile(filename : String);
     end;
 
 var
@@ -28,26 +30,14 @@ implementation
 
 
 procedure TTestParser.TestHookUp;
-var
-    csv : TStringList;
-    s : String;
 begin
-    csv := TStringList.Create();
-    csv.Delimiter := ',';
-    csv.QuoteChar := '"';
-    csv.LoadFromFile('test1.csv');
-
-    for s in csv do
-    begin
-        WriteLn(s);
-    end;
-
     par := TPsionOOParserWrap.Create();
-    par._AddToken(tknString, 'Hello');
-    par._AddToken(tknEOF, '');
+    par._AddTokensFromFile('test1.csv');
 
-    Check(par.Tokens[0].TType = tknString, 'Incorrect token found.');
-    CheckEquals(par.Tokens.Count, 2, 'Incorrect number of tokens.');
+    WriteLn(par.Tokens[1].TType);
+
+    Check(par.Tokens[1].TType = tknString, 'Incorrect token found.');
+    CheckEquals(par.Tokens.Count, 3, 'Incorrect number of tokens.');
     // AssertEquals('Incorrect number of tokens.', par.Tokens.Count, 1);
     FreeAndNil(par);
 end;
@@ -55,6 +45,35 @@ end;
 procedure TPsionOOParserWrap._AddToken(toktype: TTokenType; tokliteral: String);
 begin
     Inherited;
+end;
+
+procedure TPsionOOParserWrap._AddTokensFromFile(filename : String);
+// TODO: Return error properly (somehow)
+var
+    csv : TStringList;
+    s, STokType, literal : String;
+    toktype : TTokenType;
+    toktypenum : Integer;
+    commapos : Integer;
+begin
+    csv := TStringList.Create();
+    // csv.Delimiter := ',';
+    // csv.QuoteChar := '"';
+    csv.LoadFromFile(filename);
+    for s in csv do
+    begin
+        commapos := pos(',', s);
+        STokType := LeftStr(s, commapos - 1);
+        toktypenum := GetEnumValue(TypeInfo(TTokenType), STokType);
+        if toktypenum = -1 then begin
+            WriteLn(format('Invalid token found (%s)', [STokType]));
+            halt(-1);
+        end;
+
+        toktype := TTokenType(toktypenum);
+        literal := Copy(s, commapos + 1);
+        self._AddToken(toktype, literal);
+    end;
 end;
 
 initialization
